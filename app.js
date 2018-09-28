75a52d5d7b12782f4b3621d4176b133dd25dfc3b
Furps <lukasznowacki@mail.com>
2018-09-28 13:07:20 +0200

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var flash = require('connect-flash');
 var session = require('express-session');
 var passport = require('passport');
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://magisterka:administrator11@ds115543.mlab.com:15543/databasemy');
+mongoose.connect('mongodb://admin:admin11@ds115543.mlab.com:15543/databasemy');
 var MongoDBStore = require('connect-mongodb-session')(session);
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -51,7 +51,7 @@ var app = express();
 app.io = require('socket.io')();
 var store = new MongoDBStore(
     {
-        uri: 'mongodb://magisterka:administrator11@ds215633.mlab.com:15633/mongodb_session',
+        uri: 'mongodb://admin:admin11@ds215633.mlab.com:15633/mongodb_session',
         collection: 'mysessions'
     });
 app.use(session({
